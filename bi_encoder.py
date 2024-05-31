753545d768d220db11512bd7d51c42c78fe477c7
Alishba Imran <44557946+alishbaimran@users.noreply.github.com>
2024-05-31 18:55:07 -0400

    Update bi_encoder.py

--- bi_encoder.py
+++ bi_encoder.py
@@ -9,17 +9,22 @@ class BiEncoder(nn.Module):
         self.query_encoder = esm2_model
         self.alphabet = esm2_alphabet
         self.batch_converter = self.alphabet.get_batch_converter()
-        self.query_encoder.eval()  # set ESM2 to evaluation mode to make sure no training (frozen)
+        self.query_encoder.eval()  #  ESM2 to evaluation mode
         for param in self.query_encoder.parameters():
             param.requires_grad = False  # freeze ESM2 parameters
         
         self.passage_encoder = BertModel.from_pretrained(bert_model_name)
         self.tokenizer = BertTokenizer.from_pretrained(bert_model_name)
 
+        # linear layer to project protein embeddings to the same dimension as BERT embeddings (TO-DO)
+        self.projection = nn.Linear(1280, 768)
+
     def forward(self, protein_sequences, descriptions):
         # encode protein sequences with ESM-2
         batch_labels, batch_strs, batch_tokens = self.batch_converter(protein_sequences)
-        with torch.no_grad():  # no gradients are calculated for ESM2
+        batch_tokens = batch_tokens.to(next(self.query_encoder.parameters()).device)  
+        
+        with torch.no_grad():  #  no gradients are calculated for ESM2
             results = self.query_encoder(batch_tokens, repr_layers=[33], return_contacts=False)
         token_representations = results["representations"][33]
         
@@ -29,14 +34,12 @@ class BiEncoder(nn.Module):
             protein_embeddings.append(token_representations[i, 1:tokens_len - 1].mean(0))
         protein_embeddings = torch.stack(protein_embeddings)
         
+        # project protein embeddings to the same dimension as BERT embeddings
+        protein_embeddings = self.projection(protein_embeddings)
+        
         # tokenize and encode descriptions with BERT
         inputs = self.tokenizer(descriptions, return_tensors='pt', padding=True, truncation=True)
+        inputs = {key: value.to(protein_embeddings.device) for key, value in inputs.items()}
         description_embeddings = self.passage_encoder(**inputs).last_hidden_state[:, 0, :]
         
         return protein_embeddings, description_embeddings
-
-# ESM-2 model
-esm2_model, esm2_alphabet = esm.pretrained.esm2_t33_650M_UR50D()
-
-# instantiate the bi-encoder model
-bi_encoder = BiEncoder(esm2_model, esm2_alphabet)
