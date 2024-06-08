d3578e45bdbc3628fec59886a9e0a739e189c960
Alishba Imran <44557946+alishbaimran@users.noreply.github.com>
2024-06-08 13:43:21 -0400

    Update train.py

--- train.py
+++ train.py
@@ -7,9 +7,10 @@ import esm
 from transformers import BertModel, BertTokenizer
 import torch.nn as nn
 import torch.optim as optim
-from torch.nn.functional import cosine_similarity
+import torch.nn.functional as F
 import wandb 
 import re
+from tqdm import tqdm
 
 wandb.init(project="protein-biencoder")
 print(torch.__version__)
@@ -18,7 +19,6 @@ print(torch.version.cuda)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Using device: {device}')
 
-#data_file = "/shared/amyxlu/data/uniref90/uniref90.fasta"
 data_file = "/home/alishbaimran/projects/protein_biencoder/output_uniref90.fasta"
 fasta_dataset = FastaDataset(data_file, cache_indices=True)
 
@@ -31,83 +31,88 @@ val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
 esm2_model, esm2_alphabet = esm.pretrained.esm2_t33_650M_UR50D()
 
-tokenizer_path = "/home/alishbaimran/projects/protein_biencoder/uniref_bpe_tokenizer.json"
-
+tokenizer_path = "/home/alishbaimran/projects/protein_biencoder/updated_uniref_bpe_tokenizer.json"
 bi_encoder = BiEncoder(esm2_model, esm2_alphabet, tokenizer_path)
 bi_encoder = bi_encoder.to(device)
 
-# DataParallel for multi-GPU
-#if torch.cuda.device_count() > 1:
-    #print(f"Using {torch.cuda.device_count()} GPUs")
-    #bi_encoder = nn.DataParallel(bi_encoder)
+# contrastive loss function and accuracy calc 
+# mult accuracy by 100
+def contrastive_loss(queries, keys, temperature=0.1):
+    b, device = queries.shape[0], queries.device
+    logits = queries @ keys.t()
+    logits = logits - logits.max(dim=-1, keepdim=True).values
+    logits /= temperature
+    loss = F.cross_entropy(logits, torch.arange(b, device=device))
 
-# contrastive loss function
-def contrastive_loss(protein_embeddings, description_embeddings, margin=1.0):
-    # pairwise cosine similarities
-    similarities = torch.mm(protein_embeddings, description_embeddings.t())
-    
-    # labels, 1 for positive pairs, 0 for negative pairs
-    batch_size = protein_embeddings.size(0)
-    target_labels = torch.eye(batch_size, device=protein_embeddings.device)
-    
-    # contrastive loss
-    pos_loss = target_labels * torch.pow(1 - similarities, 2)
-    neg_loss = (1 - target_labels) * torch.pow(torch.clamp(similarities - margin, min=0.0), 2)
-    loss = torch.mean(pos_loss + neg_loss)
-    
-    return loss
+    # calc accuracy
+    pred_positive_idx = torch.argmax(logits, dim=1)
+    correct = torch.eq(pred_positive_idx, torch.arange(b, device=device)).type(torch.float)
+    acc = correct.sum() / b
+
+    return loss, acc
 
 # Set up optimizer
 optimizer = optim.Adam(bi_encoder.parameters(), lr=1e-5)  
 
-num_epochs = 100
+num_epochs = 150
 for epoch in range(num_epochs):
     bi_encoder.train()
     train_loss = 0.0
-    for batch in train_loader:
-        descs, seqs = batch
-        
-        optimizer.zero_grad()
-
-     
-        protein_sequences = [(f"protein_{i}", seq) for i, seq in enumerate(seqs)]
-
-        #print(f"Batch size: {len(protein_sequences)}, Protein sequence lengths: {[len(seq) for _, seq in protein_sequences]}")
-
-        protein_embeddings, description_embeddings = bi_encoder(protein_sequences, list(descs))
-
-        #print(f"Protein embeddings shape: {protein_embeddings.shape}, Description embeddings shape: {description_embeddings.shape}")
-
-        loss = contrastive_loss(protein_embeddings, description_embeddings)
-        
-        loss.backward()
-        optimizer.step()
-        
-        train_loss += loss.item()
+    train_acc = 0.0
     
+    with tqdm(total=len(train_loader), desc=f"Epoch {epoch+1}/{num_epochs} - Training") as pbar:
+        for batch in train_loader:
+            descs, seqs = batch
+            
+            optimizer.zero_grad()
+
+            protein_sequences = [(f"protein_{i}", seq) for i, seq in enumerate(seqs)]
+            protein_embeddings, description_embeddings = bi_encoder(protein_sequences, list(descs))
+
+            loss, acc = contrastive_loss(protein_embeddings, description_embeddings)
+            
+            loss.backward()
+            optimizer.step()
+            
+            train_loss += loss.item()
+            train_acc += acc.item()
+            
+            pbar.update(1)
+            pbar.set_postfix({'loss': train_loss / pbar.n, 'accuracy': train_acc / pbar.n})
+
     avg_train_loss = train_loss / len(train_loader)
-    print(f"Epoch: {epoch}, Training Loss: {avg_train_loss}")
+    avg_train_acc = train_acc / len(train_loader)
+    print(f"Epoch: {epoch+1}, Training Loss: {avg_train_loss}, Training Accuracy: {avg_train_acc}")
 
     # validation loop
     bi_encoder.eval()
     val_loss = 0.0
-    with torch.no_grad():
-        for batch in val_loader:
-            descs, seqs = batch
-            protein_sequences = [(f"protein_{i}", seq) for i, seq in enumerate(seqs)]
-            protein_embeddings, description_embeddings = bi_encoder(protein_sequences, list(descs))
+    val_acc = 0.0
+    with tqdm(total=len(val_loader), desc=f"Epoch {epoch+1}/{num_epochs} - Validation") as pbar:
+        with torch.no_grad():
+            for batch in val_loader:
+                descs, seqs = batch
+                protein_sequences = [(f"protein_{i}", seq) for i, seq in enumerate(seqs)]
+                protein_embeddings, description_embeddings = bi_encoder(protein_sequences, list(descs))
 
-            loss = contrastive_loss(protein_embeddings, description_embeddings)
-            val_loss += loss.item()
+                loss, acc = contrastive_loss(protein_embeddings, description_embeddings)
+                val_loss += loss.item()
+                val_acc += acc.item()
+                
+                pbar.update(1)
+                pbar.set_postfix({'loss': val_loss / pbar.n, 'accuracy': val_acc / pbar.n})
     
     avg_val_loss = val_loss / len(val_loader)
-    print(f"Epoch: {epoch}, Validation Loss: {avg_val_loss}")
+    avg_val_acc = val_acc / len(val_loader)
+    print(f"Epoch: {epoch+1}, Validation Loss: {avg_val_loss}, Validation Accuracy: {avg_val_acc}")
 
     # log to wandb
     wandb.log({
        "epoch": epoch,
        "train_loss": avg_train_loss,
+       "train_accuracy": avg_train_acc,
        "val_loss": avg_val_loss,
+       "val_accuracy": avg_val_acc,
      }, step=epoch)
 
 wandb.finish()
